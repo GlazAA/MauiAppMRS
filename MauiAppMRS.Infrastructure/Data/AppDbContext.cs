f89c6c0c0426f59788c0d18751859c948c04de88
GlazAA <ag9265227733@gmail.com>
2025-10-16 14:06:22 +0300

    ADDED: BD models. STATUS: working. ERRORS: 0

--- MauiAppMRS.Infrastructure/Data/AppDbContext.cs
+++ MauiAppMRS.Infrastructure/Data/AppDbContext.cs
@@ -1,4 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using MauiAppMRS.Core.Entities; 
+
+
 
 namespace MauiAppMRS.Infrastructure.Data
 {
@@ -8,6 +11,31 @@ namespace MauiAppMRS.Infrastructure.Data
         {
         }
 
-        // ѕока без DbSet - добавим после проверки запуска
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<EquipmentType> EquipmentTypes { get; set; }
+        public DbSet<MaintenanceType> MaintenanceTypes { get; set; }
+        public DbSet<Installation> Installations { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<OrganizationAddress> OrganizationAddresses { get; set; }
+        public DbSet<OrganizationEmployee> OrganizationEmployees { get; set; }
+        public DbSet<Facility> Facilities { get; set; }
+        public DbSet<FacilitySystem> FacilitySystems { get; set; }
+        public DbSet<SystemEquipmentType> SystemEquipmentTypes { get; set; }
+        public DbSet<EquipmentModel> EquipmentModels { get; set; }
+        public DbSet<ChecklistTemplate> ChecklistTemplates { get; set; }
+        public DbSet<FieldType> FieldTypes { get; set; }
+        public DbSet<ChecklistTemplateItem> ChecklistTemplateItems { get; set; }
+        public DbSet<Checklist> Checklists { get; set; }
+        public DbSet<ChecklistResponse> ChecklistResponses { get; set; }
+        public DbSet<MaintenanceHistory> MaintenanceHistories { get; set; }
+        public DbSet<MediaFile> MediaFiles { get; set; }
+        public DbSet<ChecklistDocumentation> ChecklistDocumentations { get; set; }
+        public DbSet<Bank> Banks { get; set; }
+        public DbSet<OwnershipForm> OwnershipForms { get; set; }
+        public DbSet<OrganizationData> OrganizationData { get; set; }
+        public DbSet<UserPersonalData> UserPersonalData { get; set; }
+        public DbSet<OrganizationHistory> OrganizationHistories { get; set; }
+
     }
 }
--- MauiAppMRS.Infrastructure/obj/Release/net9.0/MauiAppMRS.Infrastructure.AssemblyInfo.cs
+++ MauiAppMRS.Infrastructure/obj/Release/net9.0/MauiAppMRS.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiAppMRS.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cca1aa5982698bb2cccb26a86191ecf9d0e7274")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiAppMRS.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiAppMRS.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
